4dc28ed83febec7639684b81a0d70cbf04f118fd
niltonpaes <47281545+niltonpaes@users.noreply.github.com>
2019-08-02 19:47:45 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// core.php holds pagination variables
+include_once 'config/core.php';
+ 
+// include database and object files
+include_once 'config/database.php';
+include_once 'objects/product.php';
+include_once 'objects/category.php';
+ 
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+ 
+$product = new Product($db);
+$category = new Category($db);
+ 
+$page_title = "Read Products";
+include_once "layout_header.php";
+ 
+// query products
+$stmt = $product->readAll($from_record_num, $records_per_page);
+ 
+// specify the page where paging is used
+$page_url = "index.php?";
+ 
+// count total rows - used for pagination
+$total_rows=$product->countAll();
+ 
+// read_template.php controls how the product list will be rendered
+include_once "read_template.php";
+ 
+// layout_footer.php holds our javascript and closing html tags
+include_once "layout_footer.php";
+?>
---
+++ layout_footer.php
@@ -0,0 +1,51 @@
+    </div>
+    <!-- /container -->
+ 
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+ 
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+<!-- bootbox library -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
+ 
+
+<script>
+// JavaScript for deleting product
+$(document).on('click', '.delete-object', function(){
+ 
+    var id = $(this).attr('delete-id');
+ 
+    bootbox.confirm({
+        message: "<h4>Are you sure?</h4>",
+        buttons: {
+            confirm: {
+                label: '<span class="glyphicon glyphicon-ok"></span> Yes',
+                className: 'btn-danger'
+            },
+            cancel: {
+                label: '<span class="glyphicon glyphicon-remove"></span> No',
+                className: 'btn-primary'
+            }
+        },
+        callback: function (result) {
+ 
+            if(result==true){
+                $.post('delete_product.php', {
+                    object_id: id
+                }, function(data){
+                    location.reload();
+                }).fail(function() {
+                    alert('Unable to delete.');
+                });
+            }
+        }
+    });
+ 
+    return false;
+});
+</script>
+
+</body>
+</html>
